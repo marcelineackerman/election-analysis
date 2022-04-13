bcf8d525b969082fd2d65685872a020919b244fc
marcelineackerman <m.ackerman135@gmail.com>
2022-04-13 19:55:41 -0400

    Add total vote counter and retrieve candidate names

--- PyPoll.py
+++ PyPoll.py
@@ -10,6 +10,7 @@
 import csv
 import os
 
+
 # Assign a variable to load a file from a path.
 
 file_to_load = os.path.join("Resources","election_results.csv")
@@ -18,11 +19,17 @@ file_to_load = os.path.join("Resources","election_results.csv")
 
 file_to_save = os.path.join("analysis","election_analysis.txt")
 
+# Initialize a total_votes variable
+
+total_votes = 0
+
+# Declare a new list for candidate names.
+
+candidate_options = []
+
 # Open the election results and read the file.
 
 with open(file_to_load) as election_data:
-
-    # To do: read and analyze data here.
 
     file_reader = csv.reader(election_data)
 
@@ -31,5 +38,31 @@ with open(file_to_load) as election_data:
     headers = next(file_reader)
 
     print(headers)
+
+    # Print each row in the CSV file.
+
+    for row in file_reader:
+
+        # Add to the total vote count.
+
+        total_votes += 1
+
+         # Print the candidate name from each row.
+
+        candidate_name = row[2]
+
+        # Add the candidate_name to the candidate_options list using the append() method
+        if candidate_name not in candidate_options:
+
+            candidate_options.append(candidate_name)
+
+# Print the total votes
+
+print(total_votes)
+
+# Print the candidate_options list.
+
+print(candidate_options)
+
 
     
