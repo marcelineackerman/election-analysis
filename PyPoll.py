13beaf7c84e4483a022eefbf6146b41a96ec5036
marcelineackerman <m.ackerman135@gmail.com>
2022-04-13 20:31:56 -0400

    Add winning candidate summary

--- PyPoll.py
+++ PyPoll.py
@@ -31,6 +31,12 @@ candidate_options = []
 
 candidate_votes = {}
 
+# Winning candidate and Winning Count Tracker
+
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 
 with open(file_to_load) as election_data:
@@ -76,11 +82,26 @@ for candidate_name in candidate_votes:
 
     vote_percentage = float(votes) / float(total_votes)*100
 
-    print(f"{candidate_name} received {vote_percentage:.1f}% of the vote.")
+    # Print out each candidate's name, vote count, and percentage of votes
 
-# Print the candidate vote dictionary.
+    if (votes > winning_count) and (vote_percentage>winning_percentage):
 
-print(candidate_votes)
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning  Vote Count: {winning_count}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
+
+
+
 
 
     
